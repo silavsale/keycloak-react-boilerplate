b64e19f19b25a222d76c7dadebbb6cff04894ef4
MoonWalker <silavsale@gmail.com>
2024-09-30 13:27:35 -0400

    add keycloak to React app

--- Keycloak/README.md
+++ Keycloak/README.md
@@ -1,1 +1,7 @@
 ### Use Docker compose file to run Keycloak with database to store data.
+
+To run docker compose:
+
+```js
+docker-compose up -d
+```
--- client/src/Keycloak.js
+++ client/src/Keycloak.js
@@ -2,8 +2,8 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
  url: "http://localhost:8080/",
- realm: "Keycloak-react-auth",
- clientId: "React-auth",
+ realm: "myrealm",
+ clientId: "React-login-app",
 });
 
 export default keycloak;
